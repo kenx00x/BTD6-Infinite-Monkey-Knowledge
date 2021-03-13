9b53457d350340b13544bea777fe71555d443f16
kenx00x <kenx00x@gmail.com>
2021-03-13 20:13:55 +0100

    Add project files.

---
+++ BTD6 Infinite Monkey Knowledge/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Unity;
+using Assets.Scripts.Unity.UI_New.Main;
+using Harmony;
+using MelonLoader;
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Knowledge.Class1), "Infinite Monkey Knowledge", "1.0.0", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace BTD6_Infinite_Monkey_Knowledge
+{
+    public class Class1 : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("Infinite Monkey Knowledge loaded!");
+        }
+        [HarmonyPatch(typeof(MainMenu), "Open")]
+        public class TitleScreen_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Game.instance.playerService.Player.Data.KnowledgePoints = 999999999;
+            }
+        }
+    }
+}
