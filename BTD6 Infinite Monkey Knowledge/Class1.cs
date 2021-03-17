417da9a4a3ee26e4f07d915c0ffe21c9cd89a5a2
kenx00x <kenx00x@gmail.com>
2021-03-17 16:57:19 +0100

    added a way to change how much monkey knowledge you have

--- BTD6 Infinite Monkey Knowledge/Class1.cs
+++ BTD6 Infinite Monkey Knowledge/Class1.cs
@@ -2,15 +2,42 @@
 using Assets.Scripts.Unity.UI_New.Main;
 using Harmony;
 using MelonLoader;
-[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Knowledge.Class1), "Infinite Monkey Knowledge", "1.0.0", "kenx00x")]
+using System.IO;
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Knowledge.Class1), "Infinite Monkey Knowledge", "1.1.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Infinite_Monkey_Knowledge
 {
     public class Class1 : MelonMod
     {
+        public static string dir = $"{Directory.GetCurrentDirectory()}\\Mods\\InfiniteMonkeyKnowledge";
+        public static string config = $"{dir}\\config.txt";
+        public static int amount = 999999999;
         public override void OnApplicationStart()
         {
             MelonLogger.Log("Infinite Monkey Knowledge loaded!");
+            Directory.CreateDirectory($"{dir}");
+            if (File.Exists(config))
+            {
+                MelonLogger.Log("Reading config file");
+                using (StreamReader sr = File.OpenText(config))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        amount = int.Parse(s.Substring(s.IndexOf(char.Parse("=")) + 1));
+                    }
+                }
+                MelonLogger.Log("Done reading");
+            }
+            else
+            {
+                MelonLogger.Log("Creating config file");
+                using (StreamWriter sw = File.CreateText(config))
+                {
+                    sw.WriteLine("Amount=999999999");
+                }
+                MelonLogger.Log("Done Creating");
+            }
         }
         [HarmonyPatch(typeof(MainMenu), "Open")]
         public class TitleScreen_Patch
@@ -18,7 +45,7 @@ namespace BTD6_Infinite_Monkey_Knowledge
             [HarmonyPostfix]
             public static void Postfix()
             {
-                Game.instance.playerService.Player.Data.KnowledgePoints = 999999999;
+                Game.instance.playerService.Player.Data.KnowledgePoints = amount;
             }
         }
     }
